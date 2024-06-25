7d040989bd8610b80a4ccb23173ca505961c85fc
dzkita <146095028+dzkita@users.noreply.github.com>
2024-06-25 14:30:54 -0300

    Iniciando la jugada ...

---
+++ upload/notas/Crates.md
@@ -0,0 +1,3 @@
+https://github.com/solana-developers/program-examples/blob/main/tokens/create-token/native/program/src/lib.rs
+https://docs.rs/spl-token-2022/latest/spl_token_2022/instruction/index.html
+https://spl.solana.com/token-2022
---
+++ upload/src/instructions/create.rs
@@ -0,0 +1,29 @@
+use {
+    borsh::{BorshDeserialize, BorshSerialize},
+    mpl_token_metadata::instruction as mpl_instruction,
+    solana_program::{
+        account_info::{next_account_info, AccountInfo},
+        entrypoint,
+        entrypoint::ProgramResult,
+        msg,
+        program::invoke,
+        program_pack::Pack,
+        pubkey::Pubkey,
+        rent::Rent,
+        system_instruction,
+        sysvar::Sysvar,
+    },
+    spl_token_2022::{instruction as token_instruction, state::Mint},
+};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct CreateTokenArgs {
+    pub token_title: String,
+    pub token_symbol: String,
+    pub token_uri: String,
+    pub token_decimals: u8,
+}
+
+impl CreateTokenArgs {
+    pub fn create_token_mint(&self, accounts: &[AccountInfo]) {}
+}
